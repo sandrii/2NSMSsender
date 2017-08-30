803ea621a7c8dea65f4f8ac2a7ca1a412c4b43a2
SAndrii <andriyshvorak@gmail.com>
2017-08-30 15:23:10 +0300

    valid connit

--- validnumber.py
+++ validnumber.py
@@ -4,11 +4,9 @@
 import re
 
 def validnumber(num):
-    if num[1:].isdigit and 10 < len(num) < 14:
+    if num[0] == '+' and num[1:].isdigit() and 10 < len(num) < 14:
         if len(num) == 9:
             num = '0' + num
-            return re.compile('(\+380|380|0380)').sub('0', num)
+        return re.compile('(\+380|380)').sub('0', num)
     else:
         print('Invalid phone number format')
-
-
