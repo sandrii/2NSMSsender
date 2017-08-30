b11b06bd1ea73d8c96276e326175d69d149a8d0b
SAndrii <andriyshvorak@gmail.com>
2017-08-30 15:35:58 +0300

    valid connit2

--- validnumber.py
+++ validnumber.py
@@ -4,7 +4,9 @@
 import re
 
 def validnumber(num):
-    if num[0] == '+' and num[1:].isdigit() and 10 < len(num) < 14:
+    if num.isdigit() and len(num) == 10:
+        return num
+    elif num[0] == '+' and num[1:].isdigit() and 9 <= len(num) < 14:
         if len(num) == 9:
             num = '0' + num
         return re.compile('(\+380|380)').sub('0', num)
