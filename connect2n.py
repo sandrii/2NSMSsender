ce9b1badf8853a23957bfccbb279125d32815ce1
SAndrii <andriyshvorak@gmail.com>
2017-09-07 13:13:44 +0300

    queue problem solved

--- connect2n.py
+++ connect2n.py
@@ -7,9 +7,10 @@ Example:
         To send sms message   - connector('at^sm=1,13,0001000A81601332547600000168,6e', sms=1)
         To get data from gate - connector('at&spr', 'Layer2')'''
 
-import telnetlib
+import telnetlib, time
 
-def connector(mes, reunt='OK', sms=0):
+def connector(mes, reunt='OK', sms=0):    
+    out = []
     tn = telnetlib.Telnet('172.16.0.11')
     tn.write('\n\r'.encode())
     tn.write('\n\r'.encode())
@@ -18,16 +19,26 @@ def connector(mes, reunt='OK', sms=0):
     tn.read_until('Password: '.encode())
     tn.write('2n\r'.encode())
     tn.read_until('OK'.encode())
-    out = []
     if sms == 1:
-        '''Activate control via the used session'''
         tn.write('at!g=a6\r'.encode())
-        tn.read_until('OK'.encode())
-        tn.write((mes + '\r').encode())
-        out.append(tn.read_until('*smsout: '.encode()).decode('utf-8'))
-        '''Deactivate sms control'''
-        tn.write('at!g=55\r'.encode())
-        tn.read_until('OK'.encode())
+        time.sleep(1)
+        res = tn.read_very_eager()
+        if res.decode('utf-8').find('OK') != -1:
+            tn.write((mes + '\r').encode())
+            out.append(tn.read_until('*smsout: '.encode()).decode('utf-8'))
+            tn.write('at!g=55\r'.encode())
+            tn.read_until('OK'.encode())
+        elif res.decode('utf-8').find('BUSY') != -1:
+            tn.write('at!g=99\r'.encode())
+            tn.read_until('OK'.encode())
+            tn.write('at!g=a6\r'.encode())
+            time.sleep(1)
+            res = tn.read_very_eager()
+            if res.decode('utf-8').find('OK') != -1:
+                tn.write((mes + '\r').encode())
+                out.append(tn.read_until('*smsout: '.encode()).decode('utf-8'))
+                tn.write('at!g=55\r'.encode())
+                tn.read_until('OK'.encode())
     else:
         tn.write((mes + '\r').encode())
         out.append(tn.read_until(reunt.encode()))
