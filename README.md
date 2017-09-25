42950a327c27f905ca109e2bc1ab9c20bff992e0
SAndrii <andriyshvorak@gmail.com>
2017-09-25 13:41:02 +0300

    Add mailing feauture, modify logger

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 Flask webapp to send messages
 
 
-![Demo](https://user-images.githubusercontent.com/30243661/30633837-d8039b8e-9df5-11e7-9335-9513c9f25626.gif)
-
 
--- project/views.py
+++ project/views.py
@@ -4,12 +4,19 @@
 # -*- coding: utf-8 -*-
 
 import unicodedata
+import glob
 from time import gmtime, strftime, localtime
 from . import app
 from flask import render_template, Blueprint, request, redirect, url_for, flash
 from .core.send import nform, topdu, validnumber
 from .core.connect2n import connector
 from .forms import AddSMSForm
+
+a = glob.glob('project\sendto\*')
+s=[]
+for i in a:
+	s.append(i.partition('project\\sendto\\')[2])
+	opt = dict(zip([i for i in range(len(a))], s))
 
 def logger(n, m):
     from time import gmtime, strftime, localtime
@@ -23,42 +30,51 @@ def flash_errors(form):
             flash('Error in the {} field - {}'.format(getattr(form, field).label.text, error), 'info')
  
 @app.route('/')
-def index():
-    form = AddSMSForm(request.form)
-    return render_template('index.html', form=form)
+def index():  
+	form = AddSMSForm(request.form)
+	return render_template('index.html', form=form, opt=opt)
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
-    form = AddSMSForm(request.form)
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            message = request.form['message']
-            s = unicodedata.name(message[0]).partition(' ')[0]
-            if (s == 'LATIN' and len(message) <= 160) or (s == 'CYRILLIC' and len(message) <=70):
-                pass
-            else:
-                flash('ERROR! message lehght must be less 160 character in english or 70 in cyrrilic ', 'error')
-                return render_template('index.html', form=form)
-            number = request.form['number']
-            if validnumber(number):
-                if request.form.get('checkbox'):
-                    dcs = 1
-                else:
-                    dcs = 0
-                with open('logger.txt', 'a') as f:
-                    f.write(logger(number, message))
-                    f.close()
-                status = connector((nform(topdu(number, message, dcs), sim=0)), sms=1)
-                if status[0].find('*smsout') == -1:
-                    flash('Sending sms, please wait', 'info')
-                    while status[0].find('*smsout') == -1:
-                        status = connector((nform(topdu(number, message), sim=0)), sms=1)
-                flash('Sms was sent to ' + number, 'success')
-                return render_template('index.html', form=form)
-            else:
-                flash('ERROR! Invalid phone number format', 'error')
-        else:
-            flash_errors(form)
-            flash('ERROR! SMS not send', 'error')
-          
-    return render_template('index.html', form=form)
+	tel = []
+	form = AddSMSForm(request.form)
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			message = request.form['message']
+			s = unicodedata.name(message[0]).partition(' ')[0]
+			if (s == 'LATIN' and len(message) <= 160) or (s == 'CYRILLIC' and len(message) <=70):
+				pass
+			else:
+				flash('ERROR! message lehght must be less 160 character in english or 70 in cyrillic ', 'error')
+				return render_template('index.html', form=form)
+			number = request.form['number']
+			if number[1:].isdigit() and validnumber(number):
+				tel.append(validnumber(number))
+			elif number in opt.values():
+				with open('project\\sendto\\' + number, 'r') as f:
+					for i in f.readlines():
+						tel.append(i.strip('\n'))
+					f.close()
+			else:
+				flash('ERROR! Invalid phone number format', 'error')
+				return render_template('index.html', form=form)
+			print(tel)
+			if request.form.get('checkbox'):
+				dcs = 1
+			else:
+				dcs = 0
+			for u in tel:
+				status = connector((nform(topdu(u, message, dcs), sim=0)), sms=1)
+				if status[0].find('*smsout') == -1:
+					flash('Sending sms, please wait', 'info')
+					while status[0].find('*smsout') == -1:
+						status = connector((nform(topdu(u, message), sim=0)), sms=1)
+				with open('logger.txt', 'a') as f:
+					f.write(logger(u, message))
+					f.close()
+				flash('Sms was sent to ' + u, 'success')
+			return render_template('index.html', form=form)
+		else:
+			flash_errors(form)
+			flash('ERROR! SMS not send', 'error')
+	return render_template('index.html', form=form)
